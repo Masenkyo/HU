58b03380902e8c6a8fb1dbf73559de44b1676f7e
Sil Wever <kimensil@hotmail.com>
2026-02-23 18:54:29 +0100

    did PROG1/2/3 and added something to PROG4 + CSC1.3

---
+++ PROG/PROG2/PROG2.py
@@ -0,0 +1,5 @@
+task1 = 0 == (1 == 2)
+task2 = 2 + (3 == 4) + 5 == 7
+task3 = (1 < -1) == (3 > 4)
+
+print(task1, task2, task3)
--- PROG/PROG2/PROG4.py
+++ PROG/PROG2/PROG4.py
@@ -1,12 +1,21 @@
 from random import randint
 
 muntStukken = [50, 20, 10, 5, 2, 1]
-geld = int(input('hoeveel geld neem je mee: '))
 prijs = randint(10, 150)
-overigeGeld = geld - prijs
 muntenLijst = []
-
 print('het product is', str(prijs))
+
+while True:
+    try:
+        geld = int(input('hoeveel geld neem je mee: '))
+        if geld < prijs:
+            print('bring more money, you too poor rn')
+        else:
+            break
+    except ValueError:
+        print('please enter a number')
+
+overigeGeld = geld - prijs
 print('je houd zoveel geld over', overigeGeld)
 for munten in muntStukken:
     overig = overigeGeld % munten
