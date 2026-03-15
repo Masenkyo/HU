3c90468348d6525f44085eb0667c959e037a9770
Masenkyo <kimensil@hotmail.com>
2026-03-15 23:21:08 +0100

    almost done with 7.5

--- PROG/PROG7/PROG5.py
+++ PROG/PROG7/PROG5.py
@@ -1,3 +1,22 @@
-import json
+from json import dump
 
-with open('overheidInfo.json' 'w') as bestand:
+jsonFile = 'overheidInfo.json'
+
+while True:
+    naam = input("Wat is je achternaam? ")
+    if naam == '':
+        break
+
+    voorl = input("Wat zijn je voorletters? ")
+    gbdatum = input("Wat is je geboortedatum? ")
+    email = input("Wat is je e-mail adres? ")
+
+    info = {
+        "naam": naam,
+        "voorletters": voorl,
+        "geb_datum": gbdatum,
+        "e-mail": email
+    }
+
+    with open(jsonFile, 'a') as f:
+        dump(info, f, indent=4)
