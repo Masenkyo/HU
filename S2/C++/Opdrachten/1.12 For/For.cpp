0af9cf4acf8607fc1e7bda5c0ca39400982ffff8
Masenkyo <kimensil@hotmail.com>
2026-09-01 13:28:39 +0200

    added 1.12 For opdracht + readme

---
+++ S2/C++/Opdrachten/1.12 For/For.cpp
@@ -0,0 +1,23 @@
+// Sil Wever | Studenten nummer : 1921198
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= i; j++) {
+            cout << '*';
+        }
+        cout << '\n';
+    }
+
+    for (int i = n -1; i >= 1; i--) {
+        for (int j = 1; j <= i; j++) {
+            cout << '*';
+        }
+        cout << '\n';
+    }
+}
---
+++ S2/C++/Opdrachten/1.12 For/README.md
@@ -0,0 +1,7 @@
+# Wat heb ik geleerd?
+
+### 1
+Ik heb geleerd wat cin doet en ik weet nu ook dat << informatie opneemt en dat >> informatie geeft aan bijv. hier variable n.
+
+### Overig
+Het meeste gedoe was proberen te kijken hoe ik de for loops zou opzetten, de rest was gewoon makkelijk, maar wel leuk!
