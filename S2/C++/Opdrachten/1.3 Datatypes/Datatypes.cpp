28805d4565bc8b4bcd60ee69a3e61e502e93349c
Masenkyo <kimensil@hotmail.com>
2026-09-01 12:51:08 +0200

    added datatypes opdracht 1.3 + readme

---
+++ S2/C++/Opdrachten/1.3 Datatypes/Datatypes.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+#define PRINT(var) cout << #var << " = " << var << '\n'
+
+void OpdrachtAB() {
+    int x = 8.2; // 8
+    int y = 18 / 5; // 3
+    
+    float e = 3.429; // 3.429
+    float f = 18 / 5; // 3
+
+    char a = 'a'; // a
+    char b = 'e'; // e
+    char c = b; // e
+
+    bool r = true; // 1
+    bool u = (1 & 0); // 0
+
+    string p = "Klaar?"; // Klaar?
+    string s = string("Nee, nog") + string(" niet"); // Nee, nog niet
+
+    PRINT(x);
+    PRINT(y);
+    PRINT(e);
+    PRINT(f);
+    PRINT(a);
+    PRINT(b);
+    PRINT(c);
+    PRINT(r);
+    PRINT(u);
+    PRINT(p);
+    PRINT(s);
+}
+
+void OpdrachtCD() {
+    int x = 8.2; // 8
+    int y = 18 / 5; // 3
+    int z = 18 / 5.0; // 3
+    int f = '8' - '0'; // 8
+    int e = 10 % 3; // 1
+    
+    float t = 18 / 5; // 3
+    float s = 18 / 5.0; // 3.6
+
+    char a = 'a'; // a
+    char b = 'e'; // e
+    char d = b; // e
+    char c = 'b' + ('Z'- 'z'); // B
+
+    bool r = true && (false || true); // 1
+    bool u = 8 >= 10; // 0
+
+    string p = "Utereg"; // Utereg - > Utereg Me Stadsie!
+    p.append(" Me Stadsie!"); // Utereg Me Stadsie!
+    string station = string("Heidel") + "berglaan"; // Heidelberglaan -> Heidelberg
+    string plaats = station.erase(10, station.size()-10); // Heidelberg
+
+    PRINT(x);
+    PRINT(y);
+    PRINT(z);
+    PRINT(f);
+    PRINT(e);
+    PRINT(t);
+    PRINT(s);
+    PRINT(a);
+    PRINT(b);
+    PRINT(d);
+    PRINT(c);
+    PRINT(r);
+    PRINT(u);
+    PRINT(p);
+    PRINT(station);
+    PRINT(plaats);
+}
+
+
+
+int main() {
+    cout << "Opdracht A+B\n\n";
+
+    OpdrachtAB();
+    
+    cout << "\nExtra Opdracht C+D\n\n";
+    
+    OpdrachtCD();
+}
---
+++ S2/C++/Opdrachten/1.3 Datatypes/README.md
@@ -0,0 +1,15 @@
+# Wat heb ik geleerd?
+
+### 1
+Als je een float variable hebt dan betekent het niet dat als je 18/5 doet dat het dan 3.6 is want 18 en 5 zijn allebij een integer, ze moeten een decimal hebben om het te kunnnen uit rekenen, maar niet allebij hoeven floats of doubles te zijn. 
+
+Als 18.0 een double is of float en 5 een integer dan gaat de rekensom alsnog goed.
+
+### 2
+Ik heb geleerd for fun hoe ik #define kan gebruiken om code overzichtelijker te maken, het hoefde niet maar ik vind dit leuk om uit te vinden. Ook leerde ik hierbij dat endl gebruiken inefficient is en ik beter \n kan gebruiken.
+
+### 3
+Ook heb ik rare dingen geleerd over hoe je Z-z kan doen en dan houd je de capital over lol, dus toen werd b een hoofdletter B.
+
+### Overig
+De rest snapte ik al door gebruik van C# alleen is het nog even wennen dat C++ van boven naar beneden leest en dat station dus ook werd aangepast omdat ik in de variable plaats het ook nog weer aanpas dus dan was het niet Heidelberglaan meer maar Heidelberg net als bij plaats.
