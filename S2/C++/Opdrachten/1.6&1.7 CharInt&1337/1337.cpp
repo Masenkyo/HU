ef2ec0a2f662b9dce2c06f6e5beea9e728f65b6c
Masenkyo <kimensil@hotmail.com>
2026-09-03 10:38:15 +0200

    completed 1337 and added readme.md

--- S2/C++/Opdrachten/1.6&1.7 CharInt&1337/1337.cpp
+++ S2/C++/Opdrachten/1.6&1.7 CharInt&1337/1337.cpp
@@ -1,13 +1,35 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-void CharInt(char c) {
-    int som = c + 32;
-    char somChar = char(som);
+char CharInt(char c) {
+    return char(c - 32);
 }
 
 int main() {
     string zin;
     cout << "Maak een zin: ";
-    cin >> zin;
+    getline(cin, zin);
+
+    for (int i = 0; i < zin.length(); i++) {
+        if (zin[i] == 'e')
+            zin[i] = '3';
+        else if (zin[i] == 'l')
+            zin[i] = '1';
+        else if (zin[i] == 'o')
+            zin[i] = '0';
+        else if (zin[i] == 't')
+            zin[i] = '7';
+        else if (zin[i] == 's')
+            zin[i] = '5';
+        else if (zin[i] == 'a')
+            zin[i] = '4';
+        else if (zin[i] == 'b')
+            zin[i] = '8';
+        else if (zin[i] == 'g')
+            zin[i] = '9';
+        else if (zin[i] >= 'a' && zin[i] <= 'z')
+            zin[i] = CharInt(zin[i]);
+    }
+    cout << zin;
 }
---
+++ S2/C++/Opdrachten/1.6&1.7 CharInt&1337/README.md
@@ -0,0 +1,10 @@
+# Wat heb ik geleerd?
+
+### 1
+Ik heb geleerd hoe ik nummers naar letters kan converten en ook andersom.
+
+### 2
+Ik heb geleerd hoe ik getline() kan gebruiken.
+
+### Overig
+De rest wist ik wel al van C# maar was alsnog leuk om mee te spelen.
